4b8349cdbca90f5e41f22e82955772719b4ecd1e
TerribileDev <vitor.terribile.anjos@gmail.com>
2024-08-20 10:15:11 -0300

    frontend

--- frontend/src/Components/Navegacao.jsx
+++ frontend/src/Components/Navegacao.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Image } from "react-bootstrap";
+import logo from '../Img/Logo.png';
+import '../CSS/Nav.css'
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contato">Contato</Link>
-          </li>
-          <li>
-            <Link to="/cadastro">Cadastro</Link>
-          </li>
-          <li>
-            <Link to="/listaUsuarios">Lista de Usuários</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <header class="shadow-fade">
+        <nav className="navbar navbar-expand-lg navbar-custom fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="#"><Image src={logo} width={150} /></a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contato">Contato</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cadastro">Cadastro</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/listaUsuarios">Lista de Usuários</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+
   );
 };
 
