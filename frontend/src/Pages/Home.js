5c08db99049efeddff61fe451fcdeb916ce64109
TerribileDev <vitor.terribile.anjos@gmail.com>
2024-08-29 16:44:04 -0300

    Home

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,11 +1,10 @@
-// Home
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
-import { Image, Container, Row, Col, Card } from "react-bootstrap";
+import { Image, Container, Row, Col } from "react-bootstrap";
 import '../CSS/Home.css';
-import video from '../Img/video1.mp4';
-import video2 from '../Img/video2.mp4';
-import video3 from '../Img/video3.mp4'
+import video6 from '../Img/video6.mp4';
+import video5 from '../Img/video5.mp4';
+import video4 from '../Img/video4.mp4';
 import foto2 from '../Img/celular.png';
 import foto3 from '../Img/ia.png';
 import foto4 from '../Img/batida.png';
@@ -19,28 +18,27 @@ import arsuporte from '../Img/ar_suporte.png';
 function Home() {
   const carouselItems = [
     {
-      videoSrc: video2,
-      title: "Batida de Carro",
-      description: "Nulla vitae elit libero, a pharetra augue mollis interdum."
-    },
-    {
-      videoSrc: video,
+      videoSrc: video4,
       title: "Inteligencia Artificial",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Com nossa IA você se sentira conectado com todo o mundo do futuro, com nossa análise em tempo real do seu carro"
     },
     {
-      videoSrc: video3,
-      title: "Concessonarias",
-      description: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+      videoSrc: video5,
+      title: "Assistência",
+      description: "A nossa assistência de encontrar oficinas próximas é reconhecida mundialmente, você não terá mas medo de não encontrar ajuda"
+    },
+    {
+      videoSrc: video6,
+      title: "Compromisso",
+      description: "Nós temos o compromisso de fazer seu carro ter o melhor desempenho em qualquer categoria"
     }
   ];
 
   const cardsData = [
-    { id: 1, price:'200', title: 'Mensal', description: 'Descrição do Card Mensal' },
-    { id: 2, price:'1.200', title: 'Anual', description: 'Descrição do Card Anual' },
-    { id: 3, price:'400', title: 'Semestral', description: 'Descrição do Card Semestral' },
+    { id: 1, price: '200', title: 'Mensal', description: 'Descrição do Card Mensal' },
+    { id: 2, price: '1.200', title: 'Anual', description: 'Descrição do Card Anual' },
+    { id: 3, price: '400', title: 'Semestral', description: 'Descrição do Card Semestral' },
   ];
-
 
   return (
     <>
@@ -58,47 +56,39 @@ function Home() {
                 height="auto"
               />
               <Carousel.Caption>
-                <h2>{item.title}</h2>
-                <p>{item.description}</p>
+                <div className="carousel-center">
+                  <h2>{item.title}</h2>
+                  <p>{item.description}</p>
+                </div>
               </Carousel.Caption>
             </Carousel.Item>
           ))}
         </Carousel>
       </div>
 
-      {/* Acesso Rapido */}
+      {/* Acesso Rápido */}
 
       <Container className="esp1 shadow-fade-up">
-        <Row className="d-flex justify-content-center">
-          <Col xs={12} md={2}>
-            <Image src={arcamera} width={150} className="mov" />
-            <div className="d-flex justify-content-center">
-              <h5>Camera</h5>
-            </div>
+        <Row className="d-flex justify-content-center text-center">
+          <Col xs={6} sm={4} md={2}>
+            <Image src={arcamera} width="100%" className="mov" />
+            <h5>Camera</h5>
           </Col>
-          <Col xs={12} md={2}>
-            <Image src={aroficina} width={150} className="mov" />
-            <div className="d-flex justify-content-center">
-              <h5>Oficinas</h5>
-            </div>
+          <Col xs={6} sm={4} md={2}>
+            <Image src={aroficina} width="100%" className="mov" />
+            <h5>Oficinas</h5>
           </Col>
-          <Col xs={12} md={2}>
-            <Image src={arprivacidade} width={150} className="mov" />
-            <div className="d-flex justify-content-center">
-              <h5>Privacidade</h5>
-            </div>
+          <Col xs={6} sm={4} md={2}>
+            <Image src={arprivacidade} width="100%" className="mov" />
+            <h5>Privacidade</h5>
           </Col>
-          <Col xs={12} md={2}>
-            <Image src={arplanos} width={150} className="mov" />
-            <div className="d-flex justify-content-center">
-              <h5>Planos</h5>
-            </div>
+          <Col xs={6} sm={4} md={2}>
+            <Image src={arplanos} width="100%" className="mov" />
+            <h5>Planos</h5>
           </Col>
-          <Col xs={12} md={2}>
-            <Image src={arsuporte} width={150} className="mov" />
-            <div className="d-flex justify-content-center">
-              <h5>Suporte</h5>
-            </div>
+          <Col xs={6} sm={4} md={2}>
+            <Image src={arsuporte} width="100%" className="mov" />
+            <h5>Suporte</h5>
           </Col>
         </Row>
       </Container>
@@ -113,44 +103,52 @@ function Home() {
               <Carousel.Item className="esp1">
                 <Row className="fade-in-image">
                   <Col xs={12} md={4} className="d-flex justify-content-center">
-                    <Image src={foto4} width={300} />
+                    <Image src={foto4} width="100%" />
                   </Col>
-                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco">
-                    <h2>Nois pika e o resto é buraco Ladrão</h2>
-                    <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
-                  </Col>
-                </Row>
-              </Carousel.Item>
-              <Carousel.Item className="esp1">
-                <Row className="fade-in-image">
-                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco">
-                    <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
-                    <h2>Nois pika e o resto é buraco Ladrão</h2>
-                  </Col>
-                  <Col xs={12} md={4} className="d-flex justify-content-center">
-                    <Image src={foto2} width={300} />
+                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco text-center">
+                    <div>
+                      <h2>Nois pika e o resto é buraco Ladrão</h2>
+                      <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
+                    </div>
                   </Col>
                 </Row>
               </Carousel.Item>
               <Carousel.Item className="esp1">
                 <Row className="fade-in-image">
-                  <Col xs={12} md={4} className="d-flex justify-content-center">
-                    <Image src={foto3} width={300} />
+                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco text-center">
+                    <div>
+                      <h2>Nois pika e o resto é buraco Ladrão</h2>
+                      <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
+                    </div>
                   </Col>
-                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco">
-                    <h2>Nois pika e o resto é buraco Ladrão</h2>
-                    <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
+                  <Col xs={12} md={4} className="d-flex justify-content-center">
+                    <Image src={foto2} width="100%" />
                   </Col>
                 </Row>
               </Carousel.Item>
               <Carousel.Item className="esp1">
                 <Row className="fade-in-image">
-                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco">
-                    <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
-                    <h2>Nois pika e o resto é buraco Ladrão</h2>
+                  <Col xs={12} md={4} className="d-flex justify-content-center">
+                    <Image src={foto3} width="100%" />
+                  </Col>
+                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco text-center">
+                    <div>
+                      <h2>Nois pika e o resto é buraco Ladrão</h2>
+                      <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
+                    </div>
+                  </Col>
+                </Row>
+              </Carousel.Item>
+              <Carousel.Item className="esp1">
+                <Row className="fade-in-image">
+                  <Col xs={12} md={8} className="d-flex align-items-center justify-content-center branco text-center">
+                    <div>
+                      <h2>Nois pika e o resto é buraco Ladrão</h2>
+                      <p>A leitura é uma habilidade essencial que abre portas para o conhecimento e o crescimento pessoal. Ela nos permite explorar diferentes mundos, culturas e perspectivas, ampliando nossa compreensão do mundo ao nosso redor.</p>
+                    </div>
                   </Col>
                   <Col xs={12} md={4} className="d-flex justify-content-center">
-                    <Image src={foto5} width={300} />
+                    <Image src={foto5} width="100%" />
                   </Col>
                 </Row>
               </Carousel.Item>
@@ -162,6 +160,9 @@ function Home() {
       {/* Planos */}
 
       <div className="card-stack-container">
+        <div className="title-container font">
+          <h2>Planos</h2>
+        </div>
         {cardsData.map((card, index) => (
           <div key={card.id} className={`card-item card-${index + 1}`}>
             <h1>{card.price}</h1>
@@ -174,6 +175,5 @@ function Home() {
     </>
   );
 }
-
 
 export default Home;
