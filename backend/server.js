6e4dee2b429150957714fdf6791f5c3e0f46330e
KoRn_Hub <victorkazalligaruti@gmail.com>
2024-09-03 10:16:24 -0300

    router corrigido tentativa 1

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // const cadastros = require('./routescadastros'); 
+const cadastros = require("./routescadastros");
 const contatos = require('./routescontatos');
 
 // Importa as rotas do backend a partir daqui.
-
-const cadastros = require("./routescadastros");
 
 const app = express();
 const port = 3001; // Defina a porta que deseja utilizar.
@@ -15,9 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// Usa as rotas do backend aqui.
-
 // app.use('/', routes);
+
 app.use('/routescadastros', cadastros);
 app.use('/routescontatos', contatos);
 
