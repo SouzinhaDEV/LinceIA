828bb1209cddc7b8a503dc221027e2056e595669
SouzinhaDev <gugu2712.rodrigues@gmail.com>
2024-09-03 10:52:09 -0300

    server.js + contatos

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cadastros = require('./routescadastros'); 
+const contatos = require('./routescontatos');
 
 const app = express();
-const port = 3001; // Defina a porta que deseja utilizar.
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-// Usa as rotas do backend aqui.
 
-app.use('/', cadastros); // Utilize as rotas de cadastros corretamente.
+app.use('/cadastros', cadastros); 
+app.use('/contatos', contatos); 
 
-// Inicia o servidor na porta definida.
 
 app.listen(port, () => {
   console.log(`Servidor está rodando na porta ${port}`);
