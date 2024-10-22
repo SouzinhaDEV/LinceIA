5ddf4c6d82e5b2e3efae1c97c9e9b17e04adc38d
TerribileDev <vitor.terribile.anjos@gmail.com>
2024-10-22 10:48:59 -0300

    inteligencia artificial formulario e pagina

--- frontend/src/Pages/Admin.js
+++ frontend/src/Pages/Admin.js
@@ -1,7 +1,6 @@
 import React from "react";
 import '../CSS/Admin.css';
 import { Container, Row, Col} from 'react-bootstrap';
-import Header from "../Components/Admin/Header_Admin/Header";
 import TabelaLogin from '../Components/TabelaLogin';
 import TabelaCadastro from '../Components/TabelaCadastro';
 import TabelaCotato from '../Components/TabelaContato';
@@ -10,7 +9,6 @@ import TabelaCotato from '../Components/TabelaContato';
 const Admin = () => {
     return (
         <div className="fundoadmin">
-            <Header />
             <Container>
                 <Row>
                     <Col>
